a36b022529187a88c5ecbc0e57c1bbd10d2a9586
Raj Singh <raj123singhmourya@gmail.com>
2024-03-01 13:55:14 +0530

    project completed

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nike Dashboard frontend designing project using Tailwind CSS
+
+![Nike Project Dashboard](
+public\Nike_Dash.png)
+
+List of using in the project
+
+- React
+- Tailwind
+- HTML
+- Post css
+- Vite
+- Visual Studio Code
+
+
+#### Special Thanks to
+**Javascript Mystry youtube channel**
+
+
+
+
+
---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-2 border font-montserrat text-lg leading-none rounded-full
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : " text-white bg-coral-red"
+    } ${fullWidth && 'w-full'}"}`}
+    >
+      {label}
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrowRightIcon'
+          className='ml-2 rounded-full w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
